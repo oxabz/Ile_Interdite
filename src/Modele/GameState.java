f4217d3ef09e5fd593ba56f5e211186e1d823e1b
oxabz <legmatt0@gmail.com>
2019-05-21 13:56:55 +0200

    Ajout Code Chamilo

--- src/Modele/GameState.java
+++ src/Modele/GameState.java
@@ -1,4 +1,62 @@
 package Modele;
 
+import java.util.HashMap;
+
 public class GameState {
+
+    /*
+    ATTRIBUTES
+     */
+
+    private HashMap<Tresor,Boolean> tresors;
+    private int niveauEau;
+
+    /*
+    CONSTRUCTOR
+     */
+
+    public GameState(int difficulte){
+
+        //Initialisation du niveau d'eau en fonction de la difficulté choisie
+        niveauEau = difficulte;
+
+        //initialisation de la liste des tresor recolté
+        tresors = new HashMap<>();
+        for (Tresor tresor :
+                Tresor.values()) {
+            tresors.put(tresor,false);
+        }
+
+    }
+
+    /*
+    METHODS
+     */
+
+
+    //Sert à sauvegarder la recuperation d'un tresor
+    public void recupererTresor(Tresor tresor){
+        Boolean t = tresors.get(tresor);
+        t = true;
+    }
+
+    //Sert a incrementer le niveau d'eau
+    public void incrementNiveau(){
+        niveauEau++;
+    }
+
+    //Donne le nombre de carte inondation a piocher
+    public int getNbDeCarte(){
+        if(niveauEau<3){
+            return 2;
+        }else if(niveauEau<6){
+            return 3;
+        }else if(niveauEau<8){
+            return 4;
+        }else if(niveauEau<10){
+            return 5;
+        }else {
+            return 0;
+        }
+    }
 }
