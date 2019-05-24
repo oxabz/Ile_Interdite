cb517807e32aa93b226904dfc15b8dddc5b2abdb
Misterjacen <37700366+MisterJacen@users.noreply.github.com>
2019-05-24 14:11:56 +0200

    Création des tuiles et ajout de methodes à Grille

--- src/Project/Modele/Tuiles/Heliport.java
+++ src/Project/Modele/Tuiles/Heliport.java
@@ -1,4 +1,14 @@
 package Project.Modele.Tuiles;
 
+import Project.Modele.Aventurier;
+
 public class Heliport extends TuileApparition {
+
+	/**
+	 * CONSTRUCTOR
+	 */
+
+	public Heliport(Aventurier aventurier) {
+		super("Heliport", aventurier);
+	}
 }
--- src/Project/Modele/Tuiles/TuileApparition.java
+++ src/Project/Modele/Tuiles/TuileApparition.java
@@ -1,6 +1,34 @@
 package Project.Modele.Tuiles;
 
+import Project.Modele.Aventurier;
 import Project.Modele.Tuile;
 
 public class TuileApparition extends Tuile {
+
+	/**
+	 * ATTRIBUTES
+	 */
+
+	private Aventurier aventurier;
+
+	/**
+	 * CONSTRUCTOR
+	 */
+
+	public TuileApparition(String nom, Aventurier aventurier) {
+		super(nom);
+		setAventurier(aventurier);
+	}
+
+	/**
+	 * GETTERS/SETTERS
+	 */
+
+	public Aventurier getAventurier() {
+		return aventurier;
+	}
+
+	private void setAventurier(Aventurier aventurier) {
+		this.aventurier = aventurier;
+	}
 }
