978db1c78b7fda9c4b8464b4a18dae917f0f640a
Misterjacen <37700366+MisterJacen@users.noreply.github.com>
2019-05-27 11:13:35 +0200

    Création du deck

--- src/Project/Modele/Deck.java
+++ src/Project/Modele/Deck.java
@@ -1,4 +1,63 @@
 package Project.Modele;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Deck {
+
+	/**
+	 * ATTRIBUTES
+	 */
+
+	private LinkedList<Carte> pioche;
+	private LinkedList<Carte> defausse;
+
+	/**
+	 * CONSTRUCTOR
+	 */
+
+	public Deck() {
+		this.pioche = new LinkedList<>();
+		this.defausse = new LinkedList<>();
+	}
+
+	/**
+	 * METHODS
+	 */
+
+	public void addCartePiocheDebut(Carte carte) {
+		this.pioche.addFirst(carte);
+	}
+
+	public void addCartePiocheFin(Carte carte) {
+		this.pioche.addLast(carte);
+	}
+
+	public void addCarteDefausseDebut(Carte carte) {
+		this.defausse.addFirst(carte);
+	}
+
+	public void addCarteDefausseFin(Carte carte) {
+		this.defausse.addLast(carte);
+	}
+
+	public void melangerCartesDefausse() {
+		Collections.shuffle(this.defausse);
+	}
+
+	public void melangerCartesPioche() {
+		Collections.shuffle(this.pioche);
+	}
+
+	/**
+	 * GETTERS
+	 */
+
+	public LinkedList<Carte> getPioche() {
+		return pioche;
+	}
+
+	public LinkedList<Carte> getDefausse() {
+		return defausse;
+	}
 }
