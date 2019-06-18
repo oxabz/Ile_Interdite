394d2b8c204bfeca63989d1be1dcd363c38a9f95
oxabz <legmatt0@gmail.com>
2019-06-18 09:26:12 +0200

    Fixing all the bugs so many bugs T_T # Veuillez entrer un message de validation pour expliquer en quoi cette fusion est # nécessaire, surtout si cela fusionne une branche amont mise à jour dans une branche de sujet. # # Les lignes commençant par '#' seront ignorées, et un message vide # abandonne la validation.

--- src/Project/views/Elements/EJoueur.java
+++ src/Project/views/Elements/EJoueur.java
@@ -1,6 +1,14 @@
 package Project.views.Elements;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class EJoueur extends JPanel {
+    private String name;
+    private Color color;
+
+    private JLabel nom;
+    private EMain main;
+
+
 }
